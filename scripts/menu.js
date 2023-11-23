af8a2c897e601dc5b4d35b27a77bb87ff30006a9
Matrasos <sh.zhenis@yandex.kz>
2023-11-23 18:08:58 +0600

    add: header, footer, main

---
+++ scripts/menu.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let burger = document.querySelector(".header__burger");
+  let menu = document.querySelector(".header__menu");
+  let body = document.querySelector("body");
+
+  burger.addEventListener("click", function () {
+    burger.classList.toggle("active");
+    menu.classList.toggle("active");
+    body.classList.toggle("lock");
+  });
+
+});
