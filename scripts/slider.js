31815c72976866b784ce753f45ee26c7ea654cd6
Matrasos <sh.zhenis@yandex.kz>
2023-11-26 03:43:15 +0600

    add: gallery

---
+++ scripts/slider.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  allowTouchMove: false,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.gallery-button-next',
+    prevEl: '.gallery-button-prev',
+  },
+
+});
