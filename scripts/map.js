d0dd8e5501f0b003c929d1d790fba4dd0e94262d
Matrasos <sh.zhenis@yandex.kz>
2023-11-28 06:33:00 +0600

    add registration

---
+++ scripts/map.js
@@ -0,0 +1,23 @@
+function init() {
+  let map = new ymaps.Map("map", {
+    center: [54.879505, 69.133392],
+    zoom: 16.5,
+  });
+
+  let placemark = new ymaps.Placemark([54.879505, 69.133392], {}, {
+    iconLayout: 'default#image',
+    iconImageHref: 'https://cdn-icons-png.flaticon.com/512/684/684908.png',
+    iconImageSize: [30, 30],
+    iconImageOffset: [-15, -35]
+  })
+
+  map.geoObjects.add(placemark)
+
+  map.controls.remove('geolocationControl'); // удаляем геолокацию
+  map.controls.remove('searchControl'); // удаляем поиск
+  map.controls.remove('trafficControl'); // удаляем контроль трафика
+  map.controls.remove('typeSelector'); // удаляем тип
+  map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+  map.controls.remove('rulerControl'); // удаляем контрол правил
+}
+ymaps.ready(init);
